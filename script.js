acc87f46ccf2fb770c8da94024eabf7b055b610c
eric1sphang-svg <eric1s.phang@gmail.com>
2025-09-27 05:40:06 +0700

    MiniProject-CodingCamp
    Mini Project Coding Camp

---
+++ script.js
@@ -0,0 +1,11 @@
+function changeToPackage(){
+    window.location.href = "package.html"
+}
+
+function changeToCall(){
+    window.location.href = "index.html"
+}
+
+function changeToAbout(){
+    window.location.href = "page2.html"
+}
